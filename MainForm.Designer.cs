f314d862a0e6228040012438b737db32726af043
Mikhail Skudarnov <NickTesfer@gmail.com>
2025-03-17 04:11:43 +0700

    Ver 0.1

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace SelectRegionForDbd
+﻿
+namespace SelectRegionForDbd
 {
     partial class MainForm
     {
@@ -29,20 +30,232 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            FrankfurtLabel = new Label();
+            FrankfurtPing = new Label();
+            IrelandLabel = new Label();
+            IrelandPing = new Label();
+            LondonLabel = new Label();
+            LondonPing = new Label();
+            MumbaiLabel = new Label();
+            MumbaiPing = new Label();
+            SeoulLabel = new Label();
+            SeoulPing = new Label();
+            SingaporeLabel = new Label();
+            SingaporePing = new Label();
+            SydneyLabel = new Label();
+            SydneyPing = new Label();
+            TokyoLabel = new Label();
+            TokyoPing = new Label();
             SuspendLayout();
+            // 
+            // FrankfurtLabel
+            // 
+            FrankfurtLabel.AutoSize = true;
+            FrankfurtLabel.Font = new Font("MuseoSansEx 500", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            FrankfurtLabel.Location = new Point(50, 50);
+            FrankfurtLabel.Name = "FrankfurtLabel";
+            FrankfurtLabel.Size = new Size(170, 23);
+            FrankfurtLabel.TabIndex = 0;
+            FrankfurtLabel.Text = "Europe (Frankfurt)";
+            // 
+            // FrankfurtPing
+            // 
+            FrankfurtPing.AutoSize = true;
+            FrankfurtPing.Font = new Font("MuseoSansEx 500", 14.25F);
+            FrankfurtPing.Location = new Point(226, 50);
+            FrankfurtPing.Name = "FrankfurtPing";
+            FrankfurtPing.Size = new Size(48, 23);
+            FrankfurtPing.TabIndex = 1;
+            FrankfurtPing.Text = "ping";
+            // 
+            // IrelandLabel
+            // 
+            IrelandLabel.AutoSize = true;
+            IrelandLabel.Font = new Font("MuseoSansEx 500", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            IrelandLabel.Location = new Point(50, 100);
+            IrelandLabel.Name = "IrelandLabel";
+            IrelandLabel.Size = new Size(150, 23);
+            IrelandLabel.TabIndex = 2;
+            IrelandLabel.Text = "Europe (Ireland)";
+            // 
+            // IrelandPing
+            // 
+            IrelandPing.AutoSize = true;
+            IrelandPing.Font = new Font("MuseoSansEx 500", 14.25F);
+            IrelandPing.Location = new Point(206, 100);
+            IrelandPing.Name = "IrelandPing";
+            IrelandPing.Size = new Size(48, 23);
+            IrelandPing.TabIndex = 3;
+            IrelandPing.Text = "ping";
+            // 
+            // LondonLabel
+            // 
+            LondonLabel.AutoSize = true;
+            LondonLabel.Font = new Font("MuseoSansEx 500", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            LondonLabel.Location = new Point(50, 150);
+            LondonLabel.Name = "LondonLabel";
+            LondonLabel.Size = new Size(157, 23);
+            LondonLabel.TabIndex = 4;
+            LondonLabel.Text = "Europe (London)";
+            // 
+            // LondonPing
+            // 
+            LondonPing.AutoSize = true;
+            LondonPing.Font = new Font("MuseoSansEx 500", 14.25F);
+            LondonPing.Location = new Point(213, 150);
+            LondonPing.Name = "LondonPing";
+            LondonPing.Size = new Size(48, 23);
+            LondonPing.TabIndex = 5;
+            LondonPing.Text = "ping";
+            // 
+            // MumbaiLabel
+            // 
+            MumbaiLabel.AutoSize = true;
+            MumbaiLabel.Font = new Font("MuseoSansEx 500", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            MumbaiLabel.Location = new Point(325, 50);
+            MumbaiLabel.Name = "MumbaiLabel";
+            MumbaiLabel.Size = new Size(195, 23);
+            MumbaiLabel.TabIndex = 6;
+            MumbaiLabel.Text = "Asia Pacific (Mumbai)";
+            // 
+            // MumbaiPing
+            // 
+            MumbaiPing.AutoSize = true;
+            MumbaiPing.Font = new Font("MuseoSansEx 500", 14.25F);
+            MumbaiPing.Location = new Point(526, 50);
+            MumbaiPing.Name = "MumbaiPing";
+            MumbaiPing.Size = new Size(48, 23);
+            MumbaiPing.TabIndex = 7;
+            MumbaiPing.Text = "ping";
+            // 
+            // SeoulLabel
+            // 
+            SeoulLabel.AutoSize = true;
+            SeoulLabel.Font = new Font("MuseoSansEx 500", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            SeoulLabel.Location = new Point(325, 100);
+            SeoulLabel.Name = "SeoulLabel";
+            SeoulLabel.Size = new Size(174, 23);
+            SeoulLabel.TabIndex = 8;
+            SeoulLabel.Text = "Asia Pacific (Seoul)";
+            // 
+            // SeoulPing
+            // 
+            SeoulPing.AutoSize = true;
+            SeoulPing.Font = new Font("MuseoSansEx 500", 14.25F);
+            SeoulPing.Location = new Point(505, 100);
+            SeoulPing.Name = "SeoulPing";
+            SeoulPing.Size = new Size(48, 23);
+            SeoulPing.TabIndex = 9;
+            SeoulPing.Text = "ping";
+            // 
+            // SingaporeLabel
+            // 
+            SingaporeLabel.AutoSize = true;
+            SingaporeLabel.Font = new Font("MuseoSansEx 500", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            SingaporeLabel.Location = new Point(325, 150);
+            SingaporeLabel.Name = "SingaporeLabel";
+            SingaporeLabel.Size = new Size(213, 23);
+            SingaporeLabel.TabIndex = 10;
+            SingaporeLabel.Text = "Asia Pacific (Singapore)";
+            // 
+            // SingaporePing
+            // 
+            SingaporePing.AutoSize = true;
+            SingaporePing.Font = new Font("MuseoSansEx 500", 14.25F);
+            SingaporePing.Location = new Point(544, 150);
+            SingaporePing.Name = "SingaporePing";
+            SingaporePing.Size = new Size(48, 23);
+            SingaporePing.TabIndex = 11;
+            SingaporePing.Text = "ping";
+            // 
+            // SydneyLabel
+            // 
+            SydneyLabel.AutoSize = true;
+            SydneyLabel.Font = new Font("MuseoSansEx 500", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            SydneyLabel.Location = new Point(325, 200);
+            SydneyLabel.Name = "SydneyLabel";
+            SydneyLabel.Size = new Size(188, 23);
+            SydneyLabel.TabIndex = 12;
+            SydneyLabel.Text = "Asia Pacific (Sydney)";
+            // 
+            // SydneyPing
+            // 
+            SydneyPing.AutoSize = true;
+            SydneyPing.Font = new Font("MuseoSansEx 500", 14.25F);
+            SydneyPing.Location = new Point(519, 200);
+            SydneyPing.Name = "SydneyPing";
+            SydneyPing.Size = new Size(48, 23);
+            SydneyPing.TabIndex = 13;
+            SydneyPing.Text = "ping";
+            // 
+            // TokyoLabel
+            // 
+            TokyoLabel.AutoSize = true;
+            TokyoLabel.Font = new Font("MuseoSansEx 500", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            TokyoLabel.Location = new Point(325, 250);
+            TokyoLabel.Name = "TokyoLabel";
+            TokyoLabel.Size = new Size(179, 23);
+            TokyoLabel.TabIndex = 14;
+            TokyoLabel.Text = "Asia Pacific (Tokyo)";
+            // 
+            // TokyoPing
+            // 
+            TokyoPing.AutoSize = true;
+            TokyoPing.Font = new Font("MuseoSansEx 500", 14.25F);
+            TokyoPing.Location = new Point(510, 250);
+            TokyoPing.Name = "TokyoPing";
+            TokyoPing.Size = new Size(48, 23);
+            TokyoPing.TabIndex = 15;
+            TokyoPing.Text = "ping";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(984, 661);
+            Controls.Add(TokyoPing);
+            Controls.Add(TokyoLabel);
+            Controls.Add(SydneyPing);
+            Controls.Add(SydneyLabel);
+            Controls.Add(SingaporePing);
+            Controls.Add(SingaporeLabel);
+            Controls.Add(SeoulPing);
+            Controls.Add(SeoulLabel);
+            Controls.Add(MumbaiPing);
+            Controls.Add(MumbaiLabel);
+            Controls.Add(LondonPing);
+            Controls.Add(LondonLabel);
+            Controls.Add(IrelandPing);
+            Controls.Add(IrelandLabel);
+            Controls.Add(FrankfurtPing);
+            Controls.Add(FrankfurtLabel);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Dead by Daylight Region Selector";
             ResumeLayout(false);
+            PerformLayout();
         }
 
+
+
         #endregion
+
+        private Label FrankfurtLabel;
+        private Label FrankfurtPing;
+        private Label IrelandLabel;
+        private Label IrelandPing;
+        private Label LondonLabel;
+        private Label LondonPing;
+        private Label MumbaiLabel;
+        private Label MumbaiPing;
+        private Label SeoulLabel;
+        private Label SeoulPing;
+        private Label SingaporeLabel;
+        private Label SingaporePing;
+        private Label SydneyLabel;
+        private Label SydneyPing;
+        private Label TokyoLabel;
+        private Label TokyoPing;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
 namespace SelectRegionForDbd
 {
     public partial class MainForm : Form
@@ -5,6 +9,44 @@ namespace SelectRegionForDbd
         public MainForm()
         {
             InitializeComponent();
+            GetData();
+        }
+
+        private void GetData()
+        {
+            GetPing("gamelift.eu-central-1.amazonaws.com", FrankfurtPing);
+            GetPing("gamelift.eu-west-1.amazonaws.com", IrelandPing);
+            GetPing("gamelift.eu-west-2.amazonaws.com", LondonPing);
+            GetPing("gamelift.ap-south-1.amazonaws.com", MumbaiPing);
+            GetPing("gamelift.ap-northeast-2.amazonaws.com", SeoulPing);
+            GetPing("gamelift.ap-southeast-1.amazonaws.com", SingaporePing);
+            GetPing("gamelift.ap-southeast-2.amazonaws.com", SydneyPing);
+            GetPing("gamelift.ap-northeast-1.amazonaws.com", TokyoPing);
+
+        }
+
+        private async void GetPing(string host, Label label)
+        {
+            int port = 443;
+            Stopwatch stopwatch = new Stopwatch();
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    stopwatch.Start();
+                    await client.ConnectAsync(host, port);
+                    stopwatch.Stop();
+
+                    label.Text = $"{stopwatch.ElapsedMilliseconds} ms";
+                    label.ForeColor = stopwatch.ElapsedMilliseconds < 100 ? Color.Green :
+                                      stopwatch.ElapsedMilliseconds < 200 ? Color.Orange : Color.Red;
+                }
+            }
+            catch (Exception)
+            {
+                label.Text = "Error";
+                label.ForeColor = Color.Red;
+            }
         }
     }
 }
