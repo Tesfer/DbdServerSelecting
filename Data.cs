f07e98f5fdf5bb16910782dcc2dd08a5f0d42a84
Mikhail Skudarnov <NickTesfer@gmail.com>
2025-04-05 03:47:25 +0700

    Ver 1.0.5

---
+++ Data.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SelectRegionForDbd
+{
+    public static class Data
+    {
+        // Поиск правил брандмауэра и отображения результата на форме
+        public static void GetFirewallRuleStatus(Label label)
+        {
+            try
+            {
+                string powerShellCommandInBound = "Get-NetFirewallRule | Where-Object { $_.DisplayName -eq 'DbdBlockRule_IN' } | Select-Object -First 1";
+                string powerShellCommandOutBound = "Get-NetFirewallRule | Where-Object { $_.DisplayName -eq 'DbdBlockRule_OUT' } | Select-Object -First 1";
+                bool resultInBound = PowerShell.Command(powerShellCommandInBound);
+                bool resultOutBound = PowerShell.Command(powerShellCommandOutBound);
+                if (resultInBound && resultOutBound)
+                {
+                    label.Text = "Rules are found";
+                    label.ForeColor = Color.Red;
+                }
+                else
+                {
+                    label.Text = "No rules found";
+                    label.ForeColor = Color.Green;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error when validating a rule: {ex.Message}");
+            }
+        }
+        // Экспорт правил брандмауэра в файлы
+        public static async Task ExportFirewallRule(string ExcludedRegion, string FullPath)
+        {
+            string url = "https://ip-ranges.amazonaws.com/ip-ranges.json";
+            string filePathIn = "IP_ranges_in.txt";
+            string filePathOut = "IP_ranges_out.txt";
+            HashSet<string> allowedRegions =
+            [
+                "us-east-2", "us-west-1", "us-west-2",
+                "ap-south-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2",
+                "ap-northeast-1", "ca-central-1", "eu-central-1", "eu-west-1",
+                "eu-west-2", "sa-east-1"
+            ];
+            try
+            {
+                using HttpClient client = new();
+                string json = await client.GetStringAsync(url);
+                using JsonDocument doc = JsonDocument.Parse(json);
+                var prefixes = doc.RootElement.GetProperty("prefixes");
+                List<string> allIps = [];
+                foreach (var entry in prefixes.EnumerateArray())
+                {
+                    string region = entry.GetProperty("region").GetString()!;
+                    if (allowedRegions.Contains(region) && region != ExcludedRegion)
+                    {
+                        if (entry.TryGetProperty("ip_prefix", out var ipPrefix) && ipPrefix.GetString()?.Contains('.') == true)
+                        {
+                            allIps.Add(ipPrefix.GetString()!);
+                        }
+                    }
+                }
+                string resultIn = string.Join(",", allIps);
+                string resultOut = string.Join(",", allIps);
+                string ruleIn = $"New-NetFirewallRule -Name \"DbdBlockRule_IN\" -DisplayName \"DbdBlockRule_IN\" -Direction Inbound -Action Block -Program \"{FullPath}\" -RemoteAddress ";
+                string ruleOut = $"New-NetFirewallRule -Name \"DbdBlockRule_OUT\" -DisplayName \"DbdBlockRule_OUT\" -Direction Out -Action Block -Program \"{FullPath}\" -RemoteAddress ";
+                resultIn = ruleIn + resultIn;
+                resultOut = ruleOut + resultOut;
+                await File.WriteAllTextAsync(filePathIn, resultIn);
+                await File.WriteAllTextAsync(filePathOut, resultOut);
+                MessageBox.Show($"The rules have been successfully exported");
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Error while requesting data: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+        // Добавление правил брандмауэра
+        public static async Task CreateFirewallRule(string ExcludedRegion, string FullPath, Label Status)
+        {
+            bool hosts = Hosts.Write(ExcludedRegion);
+            if (hosts)
+            {
+                string url = "https://ip-ranges.amazonaws.com/ip-ranges.json";
+                string scriptPathIn = "firewall_in.ps1";
+                string scriptPathOut = "firewall_out.ps1";
+                HashSet<string> allowedRegions =
+                [
+                    "us-east-2", "us-west-1", "us-west-2",
+                    "ap-south-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2",
+                    "ap-northeast-1", "ca-central-1", "eu-central-1", "eu-west-1",
+                    "eu-west-2", "sa-east-1"
+                ];
+                try
+                {
+                    using HttpClient client = new();
+                    string json = await client.GetStringAsync(url);
+                    using JsonDocument doc = JsonDocument.Parse(json);
+                    var prefixes = doc.RootElement.GetProperty("prefixes");
+                    List<string> allIps = [];
+                    foreach (var entry in prefixes.EnumerateArray())
+                    {
+                        string region = entry.GetProperty("region").GetString()!;
+                        if (allowedRegions.Contains(region) && region != ExcludedRegion)
+                        {
+                            if (entry.TryGetProperty("ip_prefix", out var ipPrefix) && ipPrefix.GetString()?.Contains('.') == true)
+                            {
+                                allIps.Add(ipPrefix.GetString()!);
+                            }
+                        }
+                    }
+                    string resultIn = string.Join(",", allIps);
+                    string resultOut = string.Join(",", allIps);
+                    string ruleIn = $"New-NetFirewallRule -Name \"DbdBlockRule_IN\" -DisplayName \"DbdBlockRule_IN\" -Direction Inbound -Action Block -Program \"{FullPath}\" -RemoteAddress ";
+                    string ruleOut = $"New-NetFirewallRule -Name \"DbdBlockRule_OUT\" -DisplayName \"DbdBlockRule_OUT\" -Direction Out -Action Block -Program \"{FullPath}\" -RemoteAddress ";
+                    resultIn = ruleIn + resultIn;
+                    resultOut = ruleOut + resultOut;
+                    await File.WriteAllTextAsync(scriptPathIn, resultIn);
+                    await File.WriteAllTextAsync(scriptPathOut, resultOut);
+                    bool resultInCommand = PowerShell.Script(scriptPathIn);
+                    bool resultOutCommand = PowerShell.Script(scriptPathOut);
+                    if (resultInCommand && resultOutCommand)
+                    {
+                        MessageBox.Show("Rules have been successfully added");
+                        Data.FlushDnsCache();
+                        Data.GetFirewallRuleStatus(Status);
+                        if (File.Exists(scriptPathIn)) File.Delete(scriptPathIn);
+                        if (File.Exists(scriptPathOut)) File.Delete(scriptPathOut);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to add rules");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    MessageBox.Show($"Error while requesting data: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+            }
+        }
+        // Удаление правил брандмауэра
+        public static void DeleteFirewallRule(string region, Label Status)
+        {
+            bool hosts = Hosts.Remove(region);
+            if (hosts)
+            {
+                string powerShellCommandInBound = "Remove-NetFirewallRule -DisplayName 'DbdBlockRule_IN'";
+                string powerShellCommandOutBound = "Remove-NetFirewallRule -DisplayName 'DbdBlockRule_OUT'";
+                PowerShell.Command(powerShellCommandInBound);
+                PowerShell.Command(powerShellCommandOutBound);
+                bool isInBoundRuleRemoved = !CheckingForTheExistenceOfRules("DbdBlockRule_IN");
+                bool isOutBoundRuleRemoved = !CheckingForTheExistenceOfRules("DbdBlockRule_OUT");
+                if (isInBoundRuleRemoved && isOutBoundRuleRemoved)
+                {
+                    MessageBox.Show("Rules have been successfully removed");
+                    FlushDnsCache();
+                    Data.GetFirewallRuleStatus(Status);
+                }
+                else
+                {
+                    MessageBox.Show("Rules not found or could not be removed");
+                }
+            }
+        }
+        // Очистка DNS кэша
+        public static void FlushDnsCache()
+        {
+            try
+            {
+                ProcessStartInfo processStartInfo = new()
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C ipconfig /flushdns",
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                };
+
+                using Process process = Process.Start(processStartInfo)!;
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+        // Проверка на существование правила
+        private static bool CheckingForTheExistenceOfRules(string ruleName)
+        {
+            string command = $"Get-NetFirewallRule | Where-Object {{ $_.DisplayName -eq '{ruleName}' }}";
+            return PowerShell.Command(command);
+        }
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -17,12 +17,30 @@ namespace SelectRegionForDbd
         public MainForm()
         {
             InitializeComponent();
-            Themes.Dark(this, btnCreateRules, btnRemoveRules, btnSelectFile, FilePath, ServersBox);
+            Themes.Dark(this, btnCreateRules, btnRemoveRules, btnExportRules, btnSelectFile, FilePath, ServersBox);
             GetPing();
-            CheckFirewallRule(Status);
+            Data.GetFirewallRuleStatus(Status);
             FilePath.Select(0, 0);
         }
-
+        // Получение задержки до разных серверов Amazon GameLift Servers
+        private void GetPing()
+        {
+            Ping("gamelift.eu-central-1.amazonaws.com", FrankfurtPing);
+            Ping("gamelift.eu-west-1.amazonaws.com", IrelandPing);
+            Ping("gamelift.eu-west-2.amazonaws.com", LondonPing);
+            Ping("gamelift.ca-central-1.amazonaws.com", CentralPing);
+            Ping("gamelift.ap-south-1.amazonaws.com", MumbaiPing);
+            Ping("gamelift.ap-northeast-2.amazonaws.com", SeoulPing);
+            Ping("gamelift.ap-southeast-1.amazonaws.com", SingaporePing);
+            Ping("gamelift.ap-southeast-2.amazonaws.com", SydneyPing);
+            Ping("gamelift.ap-northeast-1.amazonaws.com", TokyoPing);
+            Ping("gamelift.us-east-2.amazonaws.com", OhioPing);
+            Ping("gamelift.us-east-1.amazonaws.com", VirginiaPing);
+            Ping("gamelift.us-west-1.amazonaws.com", CaliforniaPing);
+            Ping("gamelift.us-west-2.amazonaws.com", OregonPing);
+            Ping("gamelift.sa-east-1.amazonaws.com", PauloPing);
+        }
+        // Асинхронный метод для пинга выбранного сервера
         private static async void Ping(string host, Label label)
         {
             try
@@ -47,253 +65,7 @@ namespace SelectRegionForDbd
                 label.ForeColor = Color.Red;
             }
         }
-
-        private void GetPing()
-        {
-            Ping("gamelift.eu-central-1.amazonaws.com", FrankfurtPing);
-            Ping("gamelift.eu-west-1.amazonaws.com", IrelandPing);
-            Ping("gamelift.eu-west-2.amazonaws.com", LondonPing);
-            Ping("gamelift.ca-central-1.amazonaws.com", CentralPing);
-            Ping("gamelift.ap-south-1.amazonaws.com", MumbaiPing);
-            Ping("gamelift.ap-northeast-2.amazonaws.com", SeoulPing);
-            Ping("gamelift.ap-southeast-1.amazonaws.com", SingaporePing);
-            Ping("gamelift.ap-southeast-2.amazonaws.com", SydneyPing);
-            Ping("gamelift.ap-northeast-1.amazonaws.com", TokyoPing);
-            Ping("gamelift.us-east-2.amazonaws.com", OhioPing);
-            Ping("gamelift.us-east-1.amazonaws.com", VirginiaPing);
-            Ping("gamelift.us-west-1.amazonaws.com", CaliforniaPing);
-            Ping("gamelift.us-west-2.amazonaws.com", OregonPing);
-            Ping("gamelift.sa-east-1.amazonaws.com", PauloPing);
-        }
-        private static void CheckFirewallRule(Label label)
-        {
-            try
-            {
-                string powerShellCommandInBound = "Get-NetFirewallRule | Where-Object { $_.DisplayName -eq 'DbdBlockRule_IN' } | Select-Object -First 1";
-                string powerShellCommandOutBound = "Get-NetFirewallRule | Where-Object { $_.DisplayName -eq 'DbdBlockRule_OUT' } | Select-Object -First 1";
-                bool resultInBound = RunPowerShellCommand(powerShellCommandInBound);
-                bool resultOutBound = RunPowerShellCommand(powerShellCommandOutBound);
-                if (resultInBound && resultOutBound)
-                {
-                    label.Text = "Rules are found";
-                    label.ForeColor = Color.Red;
-                }
-                else
-                {
-                    label.Text = "No rules found";
-                    label.ForeColor = Color.Green;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ошибка при проверке правила: {ex.Message}");
-            }
-        }
-
-        private static bool RunPowerShellCommand(string command)
-        {
-            try
-            {
-                ProcessStartInfo pro = new()
-                {
-                    FileName = "powershell.exe",
-                    Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"{command}\"",
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    Verb = "runas"
-                };
-
-                using Process process = Process.Start(pro)!;
-                if (process == null) return false;
-                using System.IO.StreamReader reader = process.StandardOutput;
-                string result = reader.ReadToEnd();
-                process.WaitForExit();
-                return !string.IsNullOrEmpty(result);
-            }
-            catch (Exception ex)
-            {
-                // Путь к логам
-                string logPath = "PowerShellLog.txt";
-                File.WriteAllTextAsync(logPath, ex.Message);
-                return false;
-            }
-        }
-
-        private void DeleteRules()
-        {
-            if (ServersBox.SelectedItem == null)
-            {
-                MessageBox.Show("Please select a region");
-                return;
-            }
-            string selectedRegion = ServersBox.SelectedItem.ToString()!;
-            bool hosts = Hosts.Remove(selectedRegion);
-            if (hosts)
-            {
-                string powerShellCommandInBound = "Remove-NetFirewallRule -DisplayName 'DbdBlockRule_IN'";
-                string powerShellCommandOutBound = "Remove-NetFirewallRule -DisplayName 'DbdBlockRule_OUT'";
-                RunPowerShellCommand(powerShellCommandInBound);
-                RunPowerShellCommand(powerShellCommandOutBound);
-                bool isInBoundRuleRemoved = !IsRulePresent("DbdBlockRule_IN");
-                bool isOutBoundRuleRemoved = !IsRulePresent("DbdBlockRule_OUT");
-                if (isInBoundRuleRemoved && isOutBoundRuleRemoved)
-                {
-                    MessageBox.Show("Rules have been successfully removed");
-                    FlushDnsCache();
-                    GetPing();
-                    CheckFirewallRule(Status);
-                }
-                else
-                {
-                    MessageBox.Show("Rules not found or could not be removed");
-                }
-            }
-        }
-
-        private async Task CreateRules(string excludedRegion)
-        {
-            bool hosts = Hosts.Write(excludedRegion);
-            if (hosts)
-            {
-                string url = "https://ip-ranges.amazonaws.com/ip-ranges.json";
-                // string filePathIn = "IP_ranges_in.txt";
-                // string filePathOut = "IP_ranges_out.txt"; 
-                string scriptPathIn = "firewall_in.ps1";
-                string scriptPathOut = "firewall_out.ps1";
-                HashSet<string> allowedRegions =
-                [
-                    "us-east-2", "us-west-1", "us-west-2",
-                "ap-south-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2",
-                "ap-northeast-1", "ca-central-1", "eu-central-1", "eu-west-1",
-                "eu-west-2", "sa-east-1"
-                ];
-
-                try
-                {
-                    using HttpClient client = new();
-                    string json = await client.GetStringAsync(url);
-                    using JsonDocument doc = JsonDocument.Parse(json);
-                    var prefixes = doc.RootElement.GetProperty("prefixes");
-                    List<string> allIps = [];
-                    foreach (var entry in prefixes.EnumerateArray())
-                    {
-                        string region = entry.GetProperty("region").GetString()!;
-                        if (allowedRegions.Contains(region) && region != excludedRegion)
-                        {
-                            if (entry.TryGetProperty("ip_prefix", out var ipPrefix) && ipPrefix.GetString()?.Contains('.') == true)
-                            {
-                                allIps.Add(ipPrefix.GetString()!);
-                            }
-                        }
-                    }
-                    string resultIn = string.Join(",", allIps);
-                    string resultOut = string.Join(",", allIps);
-                    string ruleIn = $"New-NetFirewallRule -Name \"DbdBlockRule_IN\" -DisplayName \"DbdBlockRule_IN\" -Direction Inbound -Action Block -Program \"{FilePath.Text}\" -RemoteAddress ";
-                    string ruleOut = $"New-NetFirewallRule -Name \"DbdBlockRule_OUT\" -DisplayName \"DbdBlockRule_OUT\" -Direction Out -Action Block -Program \"{FilePath.Text}\" -RemoteAddress ";
-                    resultIn = ruleIn + resultIn;
-                    resultOut = ruleOut + resultOut;
-                    // Сохраняем в файл
-                    // await File.WriteAllTextAsync(filePathIn, resultIn);
-                    // await File.WriteAllTextAsync(filePathOut, resultOut);
-                    await File.WriteAllTextAsync(scriptPathIn, resultIn);
-                    await File.WriteAllTextAsync(scriptPathOut, resultOut);
-                    bool resultInCommand = RunPowerShellScript(scriptPathIn);
-                    bool resultOutCommand = RunPowerShellScript(scriptPathOut);
-                    if (resultInCommand && resultOutCommand)
-                    {
-                        MessageBox.Show("Rules have been successfully added");
-                        FlushDnsCache();
-                        GetPing();
-                        CheckFirewallRule(Status);
-                        // Удаляем файлы после выполнения
-                        // if (File.Exists(filePathIn)) File.Delete(filePathIn);
-                        // if (File.Exists(filePathOut)) File.Delete(filePathOut);
-                        if (File.Exists(scriptPathIn)) File.Delete(scriptPathIn);
-                        if (File.Exists(scriptPathOut)) File.Delete(scriptPathOut);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Failed to add rules");
-                    }
-                }
-                catch (HttpRequestException ex)
-                {
-                    MessageBox.Show($"Error while requesting data: {ex.Message}");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}");
-                }
-            }
-        }
-
-        private static bool RunPowerShellScript(string scriptPath)
-        {
-            ProcessStartInfo psi = new()
-            {
-                FileName = "powershell.exe",
-                Arguments = $"-NoProfile -ExecutionPolicy Bypass -File \"{scriptPath}\"",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                Verb = "runas"
-            };
-
-            try
-            {
-                using Process process = Process.Start(psi)!;
-                if (process != null)
-                {
-                    string output = process.StandardOutput.ReadToEnd();
-                    string error = process.StandardError.ReadToEnd();
-                    process.WaitForExit();
-
-                    if (!string.IsNullOrWhiteSpace(error))
-                    {
-                        return false;
-                    }
-                    return true;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex.Message}");
-            }
-            return false;
-        }
-
-
-        private static void FlushDnsCache()
-        {
-            try
-            {
-                ProcessStartInfo processStartInfo = new()
-                {
-                    FileName = "cmd.exe", 
-                    Arguments = "/C ipconfig /flushdns", 
-                    CreateNoWindow = true, 
-                    UseShellExecute = false, 
-                    RedirectStandardOutput = true 
-                };
-
-                using Process process = Process.Start(processStartInfo)!;
-                process.WaitForExit();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error occurred: {ex.Message}");
-            }
-        }
-
-        private static bool IsRulePresent(string ruleName)
-        {
-            string command = $"Get-NetFirewallRule | Where-Object {{ $_.DisplayName -eq '{ruleName}' }}";
-            return RunPowerShellCommand(command);
-        }
-
+        // Кнопка выбора пути к файлу DeadByDaylight-Win64-Shipping.exe
         private void BtnSelectFile_Click(object sender, EventArgs e)
         {
             openFileDialog.Title = "Select file DeadByDaylight-Win64-Shipping.exe";
@@ -304,7 +76,7 @@ namespace SelectRegionForDbd
                 FilePath.Text = $"{filePath}";
             }
         }
-
+        // Кнопка создания правил
         private async void BtnCreateRules_Click(object sender, EventArgs e)
         {
             if (ServersBox.SelectedItem == null)
@@ -318,12 +90,37 @@ namespace SelectRegionForDbd
                 return;
             }
             string selectedRegion = ServersBox.SelectedItem.ToString()!;
-            await CreateRules(selectedRegion);
+            await Data.CreateFirewallRule(selectedRegion, FilePath.Text, Status);
+            GetPing();
         }
-
+        // Кнопка удаления правил
         private void BtnRemoveRules_Click(object sender, EventArgs e)
         {
-            DeleteRules();
+            if (ServersBox.SelectedItem == null)
+            {
+                MessageBox.Show("Please select the region to be deleted");
+                return;
+            }
+            string selectedRegion = ServersBox.SelectedItem.ToString()!;
+            Data.DeleteFirewallRule(selectedRegion, Status);
+            GetPing();
+        }
+        // Кнопка экспорта правил
+        private async void BtnExportRules_Click(object sender, EventArgs e)
+        {
+            if (ServersBox.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a region");
+                return;
+            }
+            if (FilePath.Text == string.Empty)
+            {
+                MessageBox.Show("Please specify the path to the executable file");
+                return;
+            }
+            string selectedRegion = ServersBox.SelectedItem.ToString()!;
+            string filePath = FilePath.Text;
+            await Data.ExportFirewallRule(selectedRegion, filePath);
         }
     }
 }
